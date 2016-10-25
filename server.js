ecff98251675d13043e7988ef733ca1df58c208e
agoge <agoge@outlook.com>
2016-10-25 18:31:42 +0000

    Changed path for node_modules directory

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
-app.use(express.static('node_modules'));
 app.use(express.static('public'));
+app.use('/static', express.static('node_modules'));
+
 
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
