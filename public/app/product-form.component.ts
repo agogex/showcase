2b8db0d50299e8b4e479efafc0cf19d43bf86f27
agogex <agoge@outlook.com>
2016-11-08 21:42:57 +0200

    Added validation for new product form

--- public/app/product-form.component.ts
+++ public/app/product-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 import { ProductService } from './product.service';
 import { Product } from './models';
